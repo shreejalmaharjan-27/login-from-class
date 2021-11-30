7dd276f1e262088a1e914024ff8f64011d226193
shreejalmaharjan-27 <shreejal@rafled.com>
2021-11-30 09:22:45 +0000

    updated

--- data.php
+++ data.php
@@ -1,5 +1,28 @@
 <?php
 $name = htmlentities($_POST['name']);
-$number = htmlentities($_POST['contact']);
+$number = htmlentities(is_numeric($_POST['contact']));
 $email = htmlentities($_POST['email']);
-echo $name;
+if($name && $number && $email) {
+    echo "<script>
+            $(\"#title-logon \").text(\"Welcome, $name\");
+            </script>
+    ";
+    echo <<<HTML
+    <table>
+    <tr>
+        <th>Name</th>
+        <th>Contact</th>
+        <th>Email</th>
+    </tr>
+    <tr>
+        <td>{$name}</td>
+        <td>{$number}</td>
+        <td>{$email}</td>
+    </tr>
+    </table>
+
+HTML;
+}
+else {
+    http_response_code(403);
+}
--- index.php
+++ index.php
@@ -5,13 +5,16 @@
             Log On form
         </title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="style.css?v=0.0.1">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     </head>
     <body>
-        <h1>Student Log In</h1>
+        <div style="display: flex; justify-content: space-between; align-items: center;">
+            <h1 id="title-logon">Student Log In</h1>
+            <div id="logout"></div>
+        </div>
         <hr>
-        <div class="wrapper-main">
+        <div class="wrapper-main" id="app">
             <form action="" method="post" onsubmit="processdata(); return false;">
                 <input name="name" type="text" placeholder="name" class="input-b" id="name" required>
                 <input name="email" type="email" placeholder="email" class="input-b" id="email" required>
@@ -20,6 +23,17 @@
             </form>
         </div>
         <script>
+            function reset() {
+                $("#title-logon").text("Student Log In");
+                $("#app").html(`
+                <form action="" method="post" onsubmit="processdata(); return false;">
+                <input name="name" type="text" placeholder="name" class="input-b" id="name" required>
+                <input name="email" type="email" placeholder="email" class="input-b" id="email" required>
+                <input name="contact" type="number" placeholder="contact" class="input-b" id="number" required>
+                <input type="submit" class="button">
+            </form>
+                `);
+            }
             function processdata() {
                 var name = $("#name").val();
                 var email = $("#email").val();
@@ -33,13 +47,33 @@
                         contact: contact
                     },
                     success: function(response) {
-                        console.log(response);
+                        $("#app").html(response);
+                        $("#logout").html('<h3 class="reset" onclick="reset();">Log Out</h3>');
                     },
                     error: function() {
-                        console.log("houston, we got a problem");
+                        $("#app").html("houston, we got a problem");
                     }
                 });
             }    
         </script>
+        <style>
+            .reset {
+                cursor: pointer;
+            }     
+            .reset:hover {
+                text-decoration: underline;
+            }
+            th {
+                padding: 0.5em 1em;
+            }
+            td {
+                padding: 0.5em 1em;
+                border-top: 1px solid #000;
+                border-bottom: 1px solid #000;
+                background: antiquewhite;
+                border-left: 25px solid antiquewhite;
+                border-right: 25px solid antiquewhite;
+            }
+        </style>
     </body>
 </html>
